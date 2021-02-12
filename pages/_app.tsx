03c16e4b18457f93e63343e802f4bea6f1ce7cab
ian <ian@MacBook-Pro.local>
2021-02-12 17:30:39 +0100

    add pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,5 +3,22 @@ import type { AppProps } from "next/app";
 import "../theme/globalStyles.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div
+      css={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundImage: "url(/background.jpg)",
+        backgroundSize: "cover",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Component {...pageProps} />
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,44 @@
+import * as React from "react";
+
+import { issuesData } from "../data";
+import { VisibleCategory } from "../types";
+import {
+  ContentWrapper,
+  IssuesCategories,
+  Window,
+  IssuesList,
+  NavigationWrapper,
+  SystemControls,
+} from "../components";
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>;
+  const [visibleCategory, setVisibleCategory] = React.useState<VisibleCategory>(
+    "all"
+  );
+
+  const visibleIssues = React.useMemo(
+    () =>
+      visibleCategory === "all"
+        ? issuesData.map((issueGroup) => issueGroup.data).flat()
+        : issuesData.find((group) => group.status === visibleCategory)?.data,
+    [visibleCategory]
+  );
+
+  return (
+    <Window>
+      <NavigationWrapper>
+        <SystemControls />
+        <IssuesCategories
+          issues={issuesData}
+          visibleCategory={visibleCategory}
+          setVisibleCategory={setVisibleCategory}
+        />
+      </NavigationWrapper>
+      <ContentWrapper>
+        {visibleIssues && <IssuesList issues={visibleIssues} />}
+      </ContentWrapper>
+    </Window>
+  );
 }
 
 export default HomePage;
