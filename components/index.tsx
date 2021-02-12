9992afc9a4ecdbf0bef6a67218a0a0d386f721e4
ian <ian@MacBook-Pro.local>
2021-02-12 17:34:48 +0100

    export all componenets from index

---
+++ components/index.tsx
@@ -0,0 +1,6 @@
+export { ContentWrapper } from "../components/ContentWrapper";
+export { IssuesCategories } from "../components/IssuesCategories";
+export { Window } from "../components/Window";
+export { IssuesList } from "../components/IssuesList";
+export { NavigationWrapper } from "../components/NavigationWrapper";
+export { SystemControls } from "../components/SystemControls";
